4bc6b2092335c6f20c6f7eab55d5790c1a1910ee
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2020-03-12 15:38:44 -0400

    adds programmatic storyboard control and navigation

---
+++ Firebase Authentication/Helper/Storyboards.swift
@@ -0,0 +1,16 @@
+//
+//  Storyboards.swift
+//  Firebase Authentication
+//
+//  Created by Nathan Hedgeman on 3/12/20.
+//  Copyright © 2020 Nathan Hedgeman. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let mainSB = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+let launchSB = UIStoryboard.init(name: "LaunchScreen", bundle: Bundle.main)
+
+var activeViewController: UIViewController?
+var navigationController: UINavigationController?
