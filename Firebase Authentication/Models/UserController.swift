3f9b6e92bf017e69dad81a2dee284c0dbbd0b456
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2020-03-19 16:21:18 -0400

    initializes current user

--- Firebase Authentication/Models/UserController.swift
+++ Firebase Authentication/Models/UserController.swift
@@ -11,21 +11,21 @@ import Firebase
 
 class UserController {
     
-    var currentUser: User?
+    var currentUser = User()
 }
 
 
 //FUNCTIONS
 extension UserController {
     // This function passes the Firebase Auth.auth().currentUser data over to the custom created user.
-    private func passAuthUserData() {
+    func passAuthUserData() {
         guard let fireUser = Auth.auth().currentUser else { return NSLog("User Controller: No current user")}
-        currentUser?.uid = fireUser.uid
-        currentUser?.emailAddress = fireUser.email!
-        currentUser?.photoURL = fireUser.photoURL
-        currentUser?.phoneNumber = fireUser.phoneNumber
-        currentUser?.displayName = fireUser.displayName
-        currentUser?.isEmailVarified = fireUser.isEmailVerified
+        currentUser.uid = fireUser.uid
+        currentUser.emailAddress = fireUser.email!
+        currentUser.photoURL = fireUser.photoURL
+        currentUser.phoneNumber = fireUser.phoneNumber
+        currentUser.displayName = fireUser.displayName
+        currentUser.isEmailVarified = fireUser.isEmailVerified
         
     }
 }
